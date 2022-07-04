483ab44cf5350377cd1b1a0aba36c3b3c1763ac6
Gigio42 <gigio42@gmail.com>
2022-07-04 17:24:03 -0300

    objetos dinamicos

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    Vector3 startingPosition;
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] [Range(0,1)] float movementFactor;
+    [SerializeField] float period = 2f;
+
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float cycles = Time.time / period;
+
+        const float tau = Mathf.PI * 2;
+        float rawSine = Mathf.Sin(cycles * tau);
+
+        movementFactor = (rawSine + 1f) / 2f;
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+    }
+}
